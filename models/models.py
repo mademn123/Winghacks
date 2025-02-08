fa3706ca0695caa22c97c4e7892f657566b92d69
Your Name <you@example.com>
2025-02-08 14:11:15 -0500

    science

--- models/models.py
+++ models/models.py
@@ -1,1 +1,11 @@
+# creating a database containing links
+import mysql.connector
+data_base = mysql.connector.connect (
+    host = "localhost",
+    user = "user",
+    password = "winghacks"
+)
 
+cursor_object = data_base.cursor()
+
+cursor_object.execute("CREATE DATABASE winghacks")
